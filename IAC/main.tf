c7d203b2f66bc6dc9878c636b1741b2ce97d1b6a
Juan Manuel Noy Taborda <juan.noy@mvm.com.co>
2022-05-18 02:28:04 -0500

    Terraform IAC added

---
+++ IAC/main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+
+  backend "azurerm" {
+
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "example-resources"
+  location = "East US 2"
+}
+
+resource "azurerm_container_registry" "cr" {
+  name                = "TestBpCr"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+}
+
+resource "azurerm_kubernetes_cluster" "kc" {
+  name                = "TestBp-aks1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "TestBpaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
+resource "azurerm_role_assignment" "ra" {
+  principal_id                     = azurerm_kubernetes_cluster.kc.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.cr.id
+  skip_service_principal_aad_check = true
+}
